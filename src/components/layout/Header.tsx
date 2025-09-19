371a361ad1c57bea7a875a2a28f20ff72dc27f8d
deadxthrre <dev@local>
2025-09-19 11:27:16 +0100

    fix(build): remove unused import 'cn' in Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Link from 'next/link'
 import { WalletButton } from '@/components/wallet/WalletButton'
-import { cn } from '@/lib/utils'
 
 export function Header() {
   return (
